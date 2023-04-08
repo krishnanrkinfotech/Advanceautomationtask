30bcbfab794040b1b5391671f20ed739b5470894
admin <admin@Krishnan>
2023-04-08 12:38:18 +0530

    Fifth commit

--- cucumberframework/test-output-thread/report.js
+++ cucumberframework/test-output-thread/report.js
@@ -1,11 +1,110 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "2f3b60ef-ef5f-4d1d-bf39-f5aa92f2db71",
+    "id": "2ca22b94-8a44-49bf-98c4-862df0982a49",
+    "feature": "To Validate the Database Functionality",
+    "scenario": "Database handling",
+    "start": 1680937525377,
+    "end": 1680937536206,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "cb137d12-3097-4c42-aef6-dc9f7e39374f",
     "feature": "To Validate the alert Functionality",
     "scenario": "Alert handling",
-    "start": 1680637454580,
-    "end": 1680637461149,
+    "start": 1680937554209,
+    "end": 1680937562122,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "e09520bc-ed0e-4915-a1f9-51bead9ec8ed",
+    "feature": "To verify the email sending",
+    "scenario": "Email sending Scenario",
+    "start": 1680937546618,
+    "end": 1680937554203,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "a2eef2ce-3ae5-4a0e-a99d-5e59acb18d9d",
+    "feature": "To validate the file uploading Scenario",
+    "scenario": "File Uploading",
+    "start": 1680937513830,
+    "end": 1680937519770,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "6c5c7062-faec-421c-acec-91c403a57c1a",
+    "feature": "To validate the negative scenario for salesforce",
+    "scenario": "Negative scenario for salesforce",
+    "start": 1680937487822,
+    "end": 1680937493605,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "8f7a27c7-008a-4e17-a926-3f51163ee2af",
+    "feature": "To pass the excel data in petstore url",
+    "scenario": "Excel scenario for petstore",
+    "start": 1680937536216,
+    "end": 1680937546612,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "7da9e015-5fb3-44cc-9841-e63bcdf947e6",
+    "feature": "To Validate the windows Functionality",
+    "scenario": "Windows  handling",
+    "start": 1680937504198,
+    "end": 1680937513808,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "1a24a5a2-272a-4bb5-b206-e737cfc21d3a",
+    "feature": "To validate the frame scenario",
+    "scenario": "Frame Handling",
+    "start": 1680937478869,
+    "end": 1680937487639,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "e71029d9-da52-431c-85e1-5b09a44f9c1a",
+    "feature": "To validate the Positive scenario for salesforce",
+    "scenario": "Positive scenario for salesforce",
+    "start": 1680937493629,
+    "end": 1680937504188,
+    "group": 1,
+    "content": "",
+    "className": "passed",
+    "tags": ""
+  },
+  {
+    "id": "62c98194-98a2-497d-b32f-7da77ab93648",
+    "feature": "To validate the PDF verification Scenario",
+    "scenario": "PDF verfication",
+    "start": 1680937519783,
+    "end": 1680937525362,
     "group": 1,
     "content": "",
     "className": "passed",
